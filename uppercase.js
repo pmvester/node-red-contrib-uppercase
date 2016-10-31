b46933f8a96a9328a6397d436122452d21cb7952
Mikael Vester <pmvester@gmail.com>
2016-10-31 13:09:05 +0100

    updated package.json and cleaned up uppercase.js

--- uppercase.js
+++ uppercase.js
@@ -2,12 +2,6 @@ module.exports = function(RED) {
     function UpperCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-	var globalContext = this.context().global;
-	if(globalContext.flicClients == null){
-		global.flicClients = {};
-		console.log("init flicClients");
-	}
-	console.log(Object.keys(globalContext));
         this.on('input', function(msg) {
             msg.payload = msg.payload.toUpperCase();
             node.send(msg);
